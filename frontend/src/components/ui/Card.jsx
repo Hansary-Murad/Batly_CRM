ec1b4f145873f78c53593e0bbc954a7f9ccc581f
Hansary-Murad <hansaryev@gmail.com>
2026-08-02 14:53:39 +0500

    build: production dist

--- frontend/src/components/ui/Card.jsx
+++ frontend/src/components/ui/Card.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { cn } from '@/lib/utils'
+import { cn } from '../../lib/utils'
 import { motion } from 'framer-motion'
 
 export const Card = React.forwardRef(({ className, hover, ...props }, ref) => (
